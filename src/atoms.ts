450dcdab478aab6ada2845d98857d0e6189c8ee0
heodokyung <limewhale@daum.net>
2022-06-03 15:08:23 +0900

    Updates

--- src/atoms.ts
+++
@@ -1,9 +0,0 @@
-import { atom } from 'recoil';
-
-/****************************************
-  * 다크모드, 라이트 모드 상태값 관리
-/***************************************/
-export const isDarkAtom = atom({
-  key: 'isDarkMode',
-  default: true,
-});
