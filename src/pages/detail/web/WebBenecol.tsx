4149412ee0eba9fd0ad695fb9b01c86f799ec4ea
heodokyung <50813871+heodokyung@users.noreply.github.com>
2023-01-25 12:49:19 +0900

    배네콜 작업내용 수정

--- src/pages/detail/web/WebBenecol.tsx
+++ src/pages/detail/web/WebBenecol.tsx
@@ -35,12 +35,7 @@ const WebBenecol = () => {
 						<li>롯데푸드 파스퇴르 베네콜 메인, 서브메인 퍼블리싱 및 GUI 구현</li>
 						<li>롯데푸드 파스퇴르 베네콜 서브 컨텐츠 퍼블리싱 및 GUI 구현</li>
 						<li>파스퇴르 베네콜 공통 마크업 가이드 배포</li>
-						<li>공통 UI 모션 개발(슬라이드, GNB, YouTube영상 제어, 모달팝업 등)</li>
-					</ul>
-				</li>
-				<li>
-					<strong>성과</strong>:
-					<ul className={pageStyle.subList}>
+						<li>공통 UI 모션 개발(슬라이드, GNB, YouTube영상 제어, 모달팝업 등)</li>										
 						<li>익스플로러(ie8~) 및 모던 브라우저 크로스브라우징 대응</li>
 						<li>Semantic Markup 퍼블리싱</li>
 					</ul>
